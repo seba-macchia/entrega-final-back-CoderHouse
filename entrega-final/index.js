69b019ef82d02874f4ace8dd48664f833e7a2bde
seba-macchia <macchiasebastianprog@gmail.com>
2024-06-29 12:55:06 +0200

    faltaba agregar passport en index

--- entrega-final/index.js
+++ entrega-final/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const passport = require('passport');
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const session = require('express-session');
